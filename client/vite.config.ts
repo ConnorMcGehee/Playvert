de315ef19ff091643989d7d187a3679b8e86e350
ConnorMcGehee <connorjames050@gmail.com>
2023-06-16 12:56:34 -0500

    Initial commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': 'http://localhost:8888',
-      '/api': 'http://localhost:8888',
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: isDev ? {
+        '/auth': 'http://localhost:8888',
+        '/api': 'http://localhost:8888',
+      } : undefined,
+    },
+  };
+});
