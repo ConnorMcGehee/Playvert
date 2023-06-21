01887c112bd0866e9434e1d7a22c812ef477818b
ConnorMcGehee <connorjames050@gmail.com>
2023-06-21 01:14:16 -0500

    Made generateString function global

--- server.ts
+++ server.ts
@@ -28,6 +28,16 @@ export enum Platform {
   Deezer
 }
 
+export const generateRandomString = function (length = 16) {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 const app = express();
 
 const PORT = process.env.PORT;
